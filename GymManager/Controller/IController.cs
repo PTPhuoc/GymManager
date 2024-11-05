1625118bc361266f83c250a55b2205638de02d7b
PTPhuoc <tanphuocphan370@gmail.com>
2024-11-05 07:06:41 +0700

    Add project files.

---
+++ GymManager/Controller/IController.cs
@@ -0,0 +1,20 @@
+﻿using GymManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.Controller
+{
+    internal interface IController
+    {
+        List<IModel> models { get; }
+        bool GetAllItem();
+        bool GetOneItem(Object id);
+        bool CreateItem(IModel model);
+        bool UpdateItem(IModel model);
+        bool DeleteItem(Object id);
+        bool ItemExist(Object id);
+    }
+}
